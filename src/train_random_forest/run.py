e360eae79cd9f04910e38aef9e9322d38711c856
bcourville2000 <bcourville2000@gmail.com>
2024-10-26 10:51:45 -0600

    implement train random forest

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -17,7 +17,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer
+from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer, OneHotEncoder
 
 import wandb
 from sklearn.ensemble import RandomForestRegressor
@@ -74,6 +74,8 @@ def go(args):
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
     # YOUR CODE HERE
+
+    sk_pipe.fit(X_train, y_train)
     ######################################
 
     # Compute r2 and MAE
@@ -88,6 +90,15 @@ def go(args):
 
     logger.info("Exporting model")
 
+    #feature engineering
+    X_val['name'] = X_val['name'].fillna('').astype('str')
+    X_val['last_review'] = X_val['last_review'].fillna('2010-01-01')
+    X_val['last_review'] = pd.to_datetime(X_val['last_review'], format='%Y-%m-%d')
+    X_val['host_name'] = X_val['host_name'].fillna('').astype('str')
+    X_val['neighbourhood_group'] = X_val['neighbourhood_group'].astype('category')
+    X_val['neighbourhood'] = X_val['neighbourhood'].astype('category')
+    X_val['room_type'] = X_val['room_type'].astype('category')
+
     # Save model package in the MLFlow sklearn format
     if os.path.exists("random_forest_dir"):
         shutil.rmtree("random_forest_dir")
@@ -97,7 +108,8 @@ def go(args):
     # HINT: use mlflow.sklearn.save_model
     signature = mlflow.models.infer_signature(X_val, y_pred)
     mlflow.sklearn.save_model(
-        # YOUR CODE HERE
+        sk_pipe,
+        "random_forest_dir",
         signature = signature,
         input_example = X_train.iloc[:5]
     )
@@ -121,7 +133,7 @@ def go(args):
     # Here we save variable r_squared under the "r2" key
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
-    # YOUR CODE HERE
+    run.summary['mae'] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -164,7 +176,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
-        # YOUR CODE HERE
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder()
     )
     ######################################
 
@@ -227,7 +240,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
 
     sk_pipe = Pipeline(
         steps =[
-        # YOUR CODE HERE
+            ("preprocessor", preprocessor),
+            ("random_forest", random_forest)
         ]
     )
 
